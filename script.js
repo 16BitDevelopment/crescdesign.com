a58eeeaab1166798e6fd82841d06b6ec2f2cd21d
16bitdevelopment <16bitdevelopment@gmail.com>
2024-11-19 20:52:23 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Mouse tracking
+
+document.addEventListener("mousemove", (event) => {
+    const {clientX, clientY} = event;
+
+    // Mouse code
+});
+
+// Scroll animations
+
+window.addEventListener("scroll", setScrollVar);
+window.addEventListener("resize", setScrollVar);
+
+function setScrollVar() {
+    const htmlEl = document.documentElement;
+    const screenHeightScrollPercent = htmlEl.scrollTop / htmlEl.clientHeight;
+
+    htmlEl.style.setProperty("--scroll", screenHeightScrollPercent * 100);
+}
+
+setScrollVar();
+
+// 3d text
+
+text3d(document.getElementById("logo"), 10, 1, 1, "#000");
+
+function text3d(el, layersNum, xPos, yPos, colour) {
+    let layers = [];
+
+    for (let layer = 0; layer < layersNum; layer += 1) {
+        layers.push(`${xPos * (layer + 1)}px ${yPos * (layer + 1)}px 0 ${colour}`);
+    }
+
+    el.style.textShadow = layers.join(",");
+}
